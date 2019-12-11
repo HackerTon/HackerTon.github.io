a50ace17c448f37f48835612014783b7cecbcca8
hackerton <alanlamkenlun@hotmail.com>
2019-12-12 02:12:05 +0800

    ammended

---
+++ precache-manifest.e0b03dcdb4350d5be397e0f3392c22c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c05fbc83bc7fc566a7a161f70a9cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6def714a1a558e94293c",
+    "url": "/static/css/main.05b08503.chunk.css"
+  },
+  {
+    "revision": "499b1d46ce72af2e1edb",
+    "url": "/static/js/2.90195044.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.90195044.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6def714a1a558e94293c",
+    "url": "/static/js/main.631f08f0.chunk.js"
+  },
+  {
+    "revision": "21e862a8b33a593a0308",
+    "url": "/static/js/runtime-main.c1c2940a.js"
+  },
+  {
+    "revision": "a46c004fd06c026066c9c9e8cd298327",
+    "url": "/static/media/me1.a46c004f.jpg"
+  },
+  {
+    "revision": "db05858cba755aacda1c9eada8fef1ea",
+    "url": "/static/media/octocat.db05858c.svg"
+  },
+  {
+    "revision": "3038cd8e9471bd2e298ef30c648b5e68",
+    "url": "/static/media/pic1.3038cd8e.jpg"
+  },
+  {
+    "revision": "32e8509afba90040bccc4257f61e4d50",
+    "url": "/static/media/pic2.32e8509a.jpg"
+  },
+  {
+    "revision": "a301810ab003de082bc11691b1ef0c31",
+    "url": "/static/media/pic3.a301810a.jpg"
+  },
+  {
+    "revision": "ed1973df3070d86a8a6c7bbe8491c3c5",
+    "url": "/static/media/ukmpano.ed1973df.jpeg"
+  }
+]);
